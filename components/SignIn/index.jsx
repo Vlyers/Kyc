38b862a209e08551c49b7f3a169ce3df6ae1554c
Vlyers <kinkin.luyten@gmail.com>
2023-11-22 16:04:46 +0700

    asu

--- components/SignIn/index.jsx
+++ components/SignIn/index.jsx
@@ -1,28 +1,12 @@
-import React from "react";
-import styles from "@/components/SignIn/index.module.css";
+import React from 'react'
+import styles from '@/components/SignIn/index.module.css'
 
 const SignIn = () => {
   return (
-    <div className={`${styles.container} flex h-screen items-center`}>
-      <div>
-        <img
-          className={"w-[650px] pl-[150px]"}
-          src={"/Stuck at Home Brainstorming.png"}
-        />
-      </div>
-
-      <div className={`${styles.frame}`}>
-      <div className={"mt-[50px]"}>
-        <img
-          className={"w-[100px]"}
-          src={"/Stuck at Home Brainstorming.png"}
-        />
-      </div>
-        <div className={`${styles.texthead} font-segoe`}>Welcomes Back!</div>
-        <div className={"font-segoe"}>Login in to Continue</div>
-      </div>
+    <div className={`${styles.container} h-screen `}>
+      <img className={'w-[650px] pl-[150px]'} src={'/Stuck at Home Brainstorming.png'}/>
     </div>
-  );
-};
+  )
+}
 
-export default SignIn;
+export default SignIn
--- components/SignUp/index.jsx
+++ components/SignUp/index.jsx
@@ -1,20 +1,65 @@
-import React from "react"
-import styles from '@/components/SignUp/index.module.css'
+import React from "react";
+import styles from "@/components/SignUp/index.module.css";
 
 const SignUp = () => {
-    return (
-        <div className={`${styles.container} h-screen `}>
-            <div>
-                <div className={`${styles.frame}`}>
-
-                </div>
+  return (
+    <div className={`${styles.container} h-screen`}>
+      <div className="flex-[6.5] flex justify-center items-center">
+        <img
+          className={`${styles.imag}`}
+          src={"/Stuck at Home Brainstorming.png"}
+          alt=""
+        />
+      </div>
+      <div className={`${styles.frame_container}`}>
+        <div className={`${styles.frame}`}>
+          <div
+            className={`${styles.head_frame} text-white text-[40px] font-segoe font-bold`}
+          >
+            Register to KYC
+          </div>
+          <div
+            className={`${styles.body_frame} font-segoe text-[20px] text-white font-bold`}
+          >
+            <div className={`flex flex-row gap-[25px] mt-[15px]`}>
+              <div className={`flex-[1]`}>
+                <div>Name</div>
                 <div>
-
+                  {" "}
+                  <input
+                    type="text"
+                    className="w-full h-10 rounded-[5px]"
+                  />{" "}
                 </div>
+              </div>
+              <div className={`flex-[1]`}>
+                <div>Username</div>
+                <div>
+                  <input type="text" className="w-full h-10 rounded-[5px] border-solid border-[1px] border-[#797979]" />
+                </div>
+              </div>
             </div>
-            <img className={ `${styles.imag}`} src="" alt="" />
-        </div>
-    )
-}
 
-export default SignUp
+            <div className="mt-[15px]">
+              <div>Email</div>
+              <div>
+                <input type="text" className="w-full h-10 rounded-[5px] border-solid border-[1px] border-[#797979]" />
+              </div>
+            </div>
+            <div className="mt-[15px] pb-[15px]">
+              <div>Password</div>
+              <div>
+                <input type="text" className="w-full h-10 rounded-[5px] border-solid border-[1px] border-[#797979]"  />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center pt-[39px] ">
+            <button className="bg-[#4EABDF] w-[283px] h-[58px] rounded-[10px] font-segoe text-white font-bold text-[24px] border-solid border-[1px] border-[#797979]">Create Account</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
