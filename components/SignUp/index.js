2ede57e292f51e6a4f75eb5b7e066d7907b4b66a
Eldico <tjandraeldico@gmail.com>
2023-11-22 11:24:02 +0700

    hgj

---
+++ components/SignUp/index.js
@@ -0,0 +1,5 @@
+export default SignUp = () => {
+    return <div>
+        asdasd
+    </div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import SignIn from '@/components/SignIn'
+import SignUp from '@/components/SignUp'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>54635654646654</div>
+    <div>
+      <SignIn />
+      <SignUp/>
+    </div>
   )
 }
